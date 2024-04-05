63d52ada33ec024f694bd1ab19a934510d97dd69
osvaldsoza <osvald.soza@gmail.com>
2024-04-05 14:49:36 -0300

    Initial Commit

---
+++ ToggleSwiftUI/ToggleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleSwiftUIApp.swift
+//  ToggleSwiftUI
+//
+//  Created by Osvaldo Souza Ferreira on 05/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ToggleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
