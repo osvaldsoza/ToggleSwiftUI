ac4874e6299eccbffc82d12328767ae60572d434
osvaldsoza <osvald.soza@gmail.com>
2024-04-05 21:32:41 -0300

    Second commit

--- ToggleSwiftUI/ContentView.swift
+++ ToggleSwiftUI/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isWiFiOn = false
+    @State var isPlay = false
+    @State var isNetworkOn = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView{
+            Toggle("Wi-Fi", systemImage: isWiFiOn ? "wifi" : "wifi.slash", isOn: $isWiFiOn)
+                .tint(.pink)
+                .font(.largeTitle)
+                .toggleStyle(.button)
+            // .labelStyle(.iconOnly)
+                .contentTransition(.symbolEffect)
+                .tabItem {
+                    Image(systemName: "wifi")
+                    Text("Wi-Fi")
+                }
+            
+            Toggle(isPlay ? "Play" : "Stop", systemImage: isPlay ? "play.circle" : "stop.circle", isOn: $isPlay)
+                .tint(.blue)
+                .font(.largeTitle)
+                .toggleStyle(.button)
+                .contentTransition(.symbolEffect)
+                .tabItem {
+                    Image(systemName: "play.circle")
+                    Text("Play")
+                }
+            
+            Toggle("Network", systemImage: isNetworkOn ? "network" : "network.slash", isOn: $isNetworkOn)
+                .tint(.blue)
+                .font(.largeTitle)
+                .toggleStyle(.button)
+                .contentTransition(.symbolEffect)
+                .tabItem {
+                    Image(systemName: "network")
+                    Text("Network")
+                }
         }
-        .padding()
     }
 }
 
